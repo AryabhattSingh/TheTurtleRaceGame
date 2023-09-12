f032614dce1dec0a6aeecfd83532da56637140ff
Aryabhatt <bhattsahab314@gmail.com>
2023-09-12 22:23:09 +0530

    Created 7 turtles. Set their shapes and colors. Added them to a turtle objects list. TO DO :- Define get_set_ready()

--- main.py
+++ main.py
@@ -10,5 +10,21 @@ while user_bet not in valid_inputs:
                                                                "with VIBGYOR.\nWhich turtle will win the race? Valid "
                                                                "inputs are V, I, B, G, Y, O, R.\nEnter a color: ")
 
+turtles = []
+colors = ["violet", "indigo", "blue", "green", "yellow", "orange", "red"]
+x_pos = -240
+y_pos = 150
+
+
+def get_set_ready(turtle_object, color, x_position, y_position):
+    pass
+
+
+for i in range(0, 7):
+    turtle_obj = turtle.Turtle()
+    get_set_ready(turtle_object=turtle_obj, color=colors[i], x_position=x_pos, y_position=y_pos)
+    y_pos -= 50
+    turtles.append(turtle_obj)
+
 
 screen.exitonclick()
