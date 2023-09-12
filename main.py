6f32985e42303e3aa6534c58fe4a54047272128e
Aryabhatt <bhattsahab314@gmail.com>
2023-09-12 22:19:36 +0530

    Added functionality to take user input through a popup. The popremains on screen till the user enters a valid input

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ import turtle
 screen = turtle.Screen()
 screen.setup(500, 400)
 
+valid_inputs = ["v", "i", "b", "g", "y", "o", "r"]
+user_bet = ""
+while user_bet not in valid_inputs:
+    user_bet = turtle.textinput(title="Make your bet.", prompt="There are 7 turtles in the race with colors matching "
+                                                               "with VIBGYOR.\nWhich turtle will win the race? Valid "
+                                                               "inputs are V, I, B, G, Y, O, R.\nEnter a color: ")
+
+
 screen.exitonclick()
